2fc23ec8bd1fcb4bb3c76abfa090168daea29d36
aboichev <aboichev@gmail.com>
2013-05-31 01:01:58 -0400

    Latest development on jq plugin

--- Kbrd/assets/js/jqKbrd.js
+++ Kbrd/assets/js/jqKbrd.js
@@ -4,19 +4,46 @@
         return (octave + 1) * 12 + noteId;
     };
 
+    var indexToWhite = function (i) {
+        return i < 5 ? i / 2 : (i + 1) / 2;
+    }
+
+    var indexToBlack = function (i) {
+        return i < 4 ? i - 1 / 2 : i / 2 - 1;
+    }
+
+    var isBlackKey = function (i) {
+        return i > 4 ? i % 2 === 0 : i % 2 !== 0;
+    };
+
+    var getFirstWhiteKeyIndex = function (dConf, o) {
+        return o == dConf.lowOct && !isBlackKey(dConf.lowIx) ? indexToWhite(dConf.lowIx): 0;
+    };
+
+    var getLastWhiteKeyIndex = function (dConf, o) {
+        return o == dConf.highOct && !isBlackKey(dConf.highIx)? indexToWhite(dConf.highIx): 6;
+    };
+
+    var getFirstBlackKeyIndex = function (dConf, o) {
+        return o == dConf.lowOct && isBlackKey(dConf.lowIx) ? indexToBlack(dConf.lowIx): 0;
+    };
+
+    var getLastBlackKeyIndex = function (dConf, o) {
+        return o == dConf.highOct && isBlackKey(dConf.highIx)? indexToBlack(dConf.highIx): 4;
+    };
+
     $.fn.kbrd = function() {
 
-        var i, o, notes, midiNum, whtElem, bkElem,
-            wkW, wkNum,
-            octStarts, octEnds, bkW,
+        var i, lastKey, o, notes,
+            wkW, wkNum, bkW,
             startOffset = 0, octOffset = 0,
-            whtIx, bkIx, bkKeyOffset,
+            whtIx, bkIx,
             bkSpan1, bkSpan2,
             dConf = {
                 lowOct: 4,
                 lowIx: 0,
                 highOct: 5,
-                highIx: 11,
+                highIx: 8,
                 wkW: 40,
                 bkW: 26
             };
@@ -29,44 +56,41 @@
             wkNum = 0;
             whtIx = 0;
             bkIx = 0;
-            octStarts = (o == dConf.lowOct) ? dConf.lowIx : 0;
-            octEnds = (o == dConf.highOct) ? dConf.highIx : 11;
-            var key, wKeys = [], wOffset = 0;
+
+            var key, wOffset = 0;
 
             notes = [];
 
             // first pass: white keys
-            for (i = 0; i < 7; i++) {                
-                
-                if (i < octStarts - 1) {
-                    continue;
-                }
-                
+            i = getFirstWhiteKeyIndex(dConf, o);
+            lastKey = getLastWhiteKeyIndex(dConf, o);
+            for (; i <= lastKey; i++) {
+
                 key = {
                     index: i,
                     target: this,
                     midiNum: getMidiNum(o, i),
                     isBlack: false
-                    //isBlack: i > 4 ? i % 2 === 0 : i % 2 !== 0
                 };
 
-                if (!key.isBlack) {                    
-                    wOffset = dConf.wkW * i;
-                    key.x = wOffset;
-                    key.width = dConf.wkW;
-                    
-                    whtElem = makeWhiteKey(key);
-                }
+                key.x = dConf.wkW * i;
+                key.width = dConf.wkW;
+                makeWhiteKey(key);
+
             }
+            wOffset = key.x + key.width;
+
             // second pass: black keys
-            for (i = 0; i < 5; i++) {
+            i = getFirstBlackKeyIndex(dConf, o);
+            lastKey = getLastBlackKeyIndex(dConf, o);
+            for (; i <= lastKey; i++) {
                     
                 key = {
                     index: i,
                     target: this,
                     midiNum: getMidiNum(o, i),
                     isBlack: true
-                    //isBlack: i > 4 ? i % 2 === 0 : i % 2 !== 0
+
                 };
                     
                 bkW = dConf.bkW;
@@ -82,70 +106,9 @@
                 }
                 key.width = dConf.bkW;                    
                 makeBlackKey(key);
-                    
             }   
             octOffset += wOffset + startOffset;
-        }        
-//                 // init notes
-//                 midiNum = getMidiNum(o, i);
-//                 notes[i] = {};
-//                 notes[i].midiNum = midiNum;
-//                 notes[i].isBlack = i > 4 ? i % 2 == 0 : i % 2 != 0;
-//                 //draw white keys
-//                 if (!notes[i].isBlack) {
-
-//                     whtElem = makeWhiteKey(this, notes[i]);
-
-//                     if (i < octStarts) {
-//                         startOffset -= whtElem[0].offsetWidth;
-//                         whtElem.hide();
-//                         continue;
-//                     }
-
-//                     if (i > octEnds) {
-//                         whtElem.hide();
-//                         continue;
-//                     }
-
-//                     wkNum += 1;
-//                     if (!wkW) {
-//                         wkW = whtElem[0].offsetWidth;
-//                     }
-//                 }
-//             }
-
-//             // second pass: draw black keys
-//             //octOffset += startOffset;
-//             bkIx = 0;
-//             for (i = 0; i < 12; i++) {
-//                 if (notes[i].isBlack && i >= octStarts) {
-//                     bkElem = makeBlackKey(this, notes[i]);
-
-//                     bkW = bkElem[0].offsetWidth;
-//                     bkSpan1 = (wkW * 3 - bkW * 2) / 3;
-//                     bkSpan2 = (wkW * 4 - bkW * 3) / 4;
-
-//                     if (i < 5) {
-//                         bkKeyOffset = bkSpan1 * (bkIx + 1) + bkW * bkIx + octOffset;
-//                     } else {
-//                         bkKeyOffset = bkSpan2 * (bkIx - 1) + (bkW * (bkIx - 2)) + (wkW * 3) + octOffset;
-//                     }
-
-// //                    if (bkKeyOffset < 0) {
-// //                        octOffset += -bkKeyOffset;
-// //                        $(this).find('div').css('left', -bkKeyOffset);
-// //                        bkKeyOffset = 0;
-// //                    }
-//                     bkElem.css('left', bkKeyOffset + 'px');
-
-//                     if (i > octEnds) {
-//                         bkElem.hide();
-//                     }
-//                     bkIx += 1;
-//                 }
-//             }
-//             octOffset += wkW * wkNum + startOffset;
-
+        }
     };
 
     var makeKey = function (key) {
